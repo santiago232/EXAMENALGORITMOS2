6a7b5f1cf33c9f83ef22bf6061331714fd54eb88
fazt <wilson@faztweb.com>
2023-09-16 11:48:06 -0600

    examen 2 de algoritmos

---
+++ examen 1.py
@@ -0,0 +1,8 @@
+def promedio_tres_numeros(num1, num2, num3):
+
+    suma = num1 + num2 + num3
+    promedio = suma / 3
+    return promedio
+
+promedio = promedio_tres_numeros(4, 8, 12)
+print(promedio)
